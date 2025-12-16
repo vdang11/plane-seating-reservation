f365fd9a27764c6ee84350216226ad4ea4c92801
Vinh <danghuuvinh94@gmail.com>
2025-12-17 07:29:13 +1100

    update

--- src/PlaneSeatingReservation.java
+++ src/PlaneSeatingReservation.java
@@ -1,61 +1,61 @@
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class PlaneSeatingReservation {
-	public static int Solution(int N, String str) {
-		Map<Integer, Set<Character>> reserved = new HashMap<>();
 
-		String[] reservedSeats = str.split(" ");
+    public static int Solution(int N, String str) {
+        Map<Integer, Set<Character>> reserved = new HashMap<>();
 
-		for (String seat : reservedSeats) {
-			char seatCharacter = seat.charAt(seat.length() - 1);
+        String[] reservedSeats = str.split(" ");
 
-			int row = Integer.parseInt(seat.substring(0, seat.length() - 1));
+        for (String seat : reservedSeats) {
+            char seatCharacter = seat.charAt(seat.length() - 1);
 
-			if (!reserved.containsKey(row)) {
-			    reserved.put(row, new HashSet<>());
-			}
-			
-			reserved.get(row).add(seatCharacter);
+            int row = Integer.parseInt(seat.substring(0, seat.length() - 1));
 
-			reserved.get(row).add(seatCharacter);
+            if (!reserved.containsKey(row)) {
+                reserved.put(row, new HashSet<>());
+            }
 
-		}
+            reserved.get(row).add(seatCharacter);
 
-		int result = 0;
+        }
 
-		for (int row = 1; row <= N; row++) {
-			if (!reserved.containsKey(row)) {
-				result += 3;
-				continue;
-			}
+        int result = 0;
 
-			Set<Character> set = reserved.get(row);
+        for (int row = 1; row <= N; row++) {
+            if (!reserved.containsKey(row)) {
+                result += 3;
+                continue;
+            }
 
-			if (!set.contains('A') && !set.contains('B') && !set.contains('C')) {
-				result++;
-			}
+            Set<Character> set = reserved.get(row);
 
-			boolean defFree = !set.contains('D') && !set.contains('E') && !set.contains('F');
-			boolean efgFree = !set.contains('E') && !set.contains('F') && !set.contains('G');
-			if (defFree || efgFree) {
-				result++;
-			}
+            if (!set.contains('A') && !set.contains('B') && !set.contains('C')) {
+                result++;
+            }
 
-			if (!set.contains('H') && !set.contains('J') && !set.contains('K')) {
-				result++;
-			}
-		}
+            boolean defFree = !set.contains('D') && !set.contains('E') && !set.contains('F');
+            boolean efgFree = !set.contains('E') && !set.contains('F') && !set.contains('G');
+            if (defFree || efgFree) {
+                result++;
+            }
 
-		return result;
+            if (!set.contains('H') && !set.contains('J') && !set.contains('K')) {
+                result++;
+            }
+        }
 
-	}
-	
-	public static void main(String[] args) {
-		String S="1A 2F 1C 5A 4G 2A";
-		int N=6;
-		System.out.println(Solution(N, S));
-	}
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        String S = "1A 2F 1C 5A 4G 2A";
+        int N = 6;
+        System.out.println(Solution(N, S));
+    }
 }
