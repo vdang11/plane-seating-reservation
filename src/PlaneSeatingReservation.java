aff93aacbe9f4ab2a424675fff78c1f776bf30ed
Vinh <danghuuvinh94@gmail.com>
2025-12-16 20:41:07 +1100

    init

---
+++ src/PlaneSeatingReservation.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PlaneSeatingReservation {
+	public static int Solution(int N, String str) {
+		Map<Integer, Set<Character>> reserved = new HashMap<>();
+
+		String[] reservedSeats = str.split(" ");
+
+		for (String seat : reservedSeats) {
+			char seatCharacter = seat.charAt(seat.length() - 1);
+
+			int row = Integer.parseInt(seat.substring(0, seat.length() - 1));
+
+			reserved.putIfAbsent(row, new HashSet<>());
+
+			reserved.get(row).add(seatCharacter);
+
+		}
+
+		int result = 0;
+
+		for (int row = 1; row <= N; row++) {
+			if (!reserved.containsKey(row)) {
+				result += 3;
+				continue;
+			}
+
+			Set<Character> set = reserved.get(row);
+
+			if (!set.contains('A') && !set.contains('B') && !set.contains('C')) {
+				result++;
+			}
+
+			boolean defFree = !set.contains('D') && !set.contains('E') && !set.contains('F');
+			boolean efgFree = !set.contains('E') && !set.contains('F') && !set.contains('G');
+			if (defFree || efgFree) {
+				result++;
+			}
+
+			if (!set.contains('H') && !set.contains('J') && !set.contains('K')) {
+				result++;
+			}
+		}
+
+		return result;
+
+	}
+	
+	public static void main(String[] args) {
+		String S="1A 2F 1C";
+		int N=2;
+		System.out.println(Solution(N, S));
+	}
+}
